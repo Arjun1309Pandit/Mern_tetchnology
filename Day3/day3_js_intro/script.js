f7c6cd3b3579602a9ed3b564338300d88c3db500
Arjun <ap4237063@gmail.com>
2024-04-04 21:13:27 +0530

    java script day 3 work with programming pathshala

---
+++ Day3/day3_js_intro/script.js
@@ -0,0 +1,182 @@
+// console.log('hello')
+
+// var message = 'hello world !';
+// var age = 18;  // loosely typed
+// var age ='done'; // loosely typed
+
+// console.log(message) 
+
+// var message ="hello";
+
+// console.log(message)
+
+// message ="else";
+
+// console.log (message)
+
+
+
+
+// function env(){
+
+// for(var i=0; i<5; i++){
+//     console.log(' ',i);
+// }
+//  console.log(i);
+// }
+
+// env();
+
+
+
+// const message='hello';
+
+// console.log(message);
+
+
+
+// let arr = ['a','b', 'c', 'd'];
+// arr[0]='abcd';  
+// console.log(arr);
+
+
+// let a = ['a','b' ,'c','d'];
+
+// let cpy = [...a];
+// if  (a==b){
+//     console.log('1.yes');
+// }else{
+//     console.log('1.no');
+// }
+// if  (a==b){
+//     console.log('2.yes');
+// }else{
+//     console.log('2s.no');
+// }
+
+
+
+// const a= 'my';
+// const b= 'name';
+// const c= 'is';
+// const d= ':';
+// const str= 'arjun';
+// const ans= `${a}${b } ${c} ${d} ${str}`;
+// const ans2= a+''+b+''+c+''+d+'';
+// console.log(ans);
+// console.log(ans2);
+
+
+
+// function print(str='nothing is here'){
+//     console.log(str);
+// }
+// print('hello');
+
+//addition of two number
+
+// function sum(a,b)
+//     {
+//         if(a && b){
+//         console.log(a+b);
+//     } 
+//         else   
+//             {
+//                 console.log(a);
+//             }
+//     }
+// sum(10,5);
+
+// let a=1;
+// if (a){
+//     console.log('yes');
+// }else{
+//     console.log('no');
+// }
+
+
+
+//function declaration
+
+// function print(){
+//     console.log('--');
+// }
+
+// function print(){
+//     console.log('++');
+// }
+
+// //function assignment (named)
+
+// const a = function print(){
+//     console.log('--');
+// }
+// // arrow function assignment
+
+// let c = () => {
+//     console.log('--');
+// }
+
+// //function assignment (annonymous)
+
+// const b = function (){
+// }
+
+// print();
+// a();
+// b();
+// c();
+
+
+// switch case 
+// let a=2;
+
+// switch(a){
+//     case a=1:{
+//         console.log(one);
+//         break;
+//     }
+//     case a=2:{
+//         console.log(one);
+//         break;
+//     }
+//     case a=3:{
+//         console.log(one);
+//         break;
+//     }
+//     default :{
+//         console.log('not defind');
+//     }
+// }
+
+
+// const obj = new Object();
+// obj [1]='one';
+// obj [2]='two';
+// obj ['name']='arjun';
+// obj [12] = 'twelve';
+// console.log(obj);
+
+// const obj ={
+//     1:'one',
+//     'firstName':'arjun',
+//     'lastName':'pandit',
+//     'age':'20',
+//     'address':'delhi',
+// };
+
+// const fullName1 = obj.firstName + '' + obj.lastName +'';
+
+// console.log(obj);
+
+
+const obj ={
+    1:'one',
+    'firstName':'arjun',
+    'lastName':'pandit',
+    'age':'20',
+    'address':'delhi',
+};
+
+const x = prompt ('please enter the property to know');
+console.log(x);
